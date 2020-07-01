376d16a4080f2a82fa82efc06be49e79635b55b6
Park Yeondu <46546637+SouleHeart@users.noreply.github.com>
2020-07-01 13:24:34 +0900

    Update vcp.tf

--- vcp.tf
+++ vcp.tf
@@ -1,11 +1,4 @@
 resource "aws_vpc" "test" {
-  cidr_block = "172.28.0.0/16"
-  assign_generated_ipv6_cidr_block = "false"
-  instance_tenancy	= "default"
-
-  tags = {
-    Name = var.tags
-  }
 }
 
 resource "aws_vpc_ipv4_cidr_block_association" "secondary_cidr" {
