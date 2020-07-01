32885d07d3f39909eae05b6eb04241ac1aa3880f
Park Yeondu <46546637+SouleHeart@users.noreply.github.com>
2020-07-01 11:41:43 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
 variable "tags" {
   type = string
 }
+
+variable "aws_region" {
+  type = string
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+}
