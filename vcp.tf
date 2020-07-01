ee614b50817c6dd5fc9772fa85a9bee5d435aa85
Park Yeondu <46546637+SouleHeart@users.noreply.github.com>
2020-07-01 12:06:00 +0900

    Update vcp.tf

--- vcp.tf
+++ vcp.tf
@@ -1,11 +1,16 @@
 resource "aws_vpc" "test" {
-  cidr_block = "172.28.0.0/16,172.30.0.0/16"
+  cidr_block = "172.28.0.0/16"
   assign_generated_ipv6_cidr_block = "false"
   instance_tenancy	= "default"
 
   tags = {
     Name = var.tags
   }
+}
+
+resource "aws_vpc_ipv4_cidr_block_association" "secondary_cidr" {
+  vpc_id     = aws_vpc.test.id
+  cidr_block = "172.30.0.0/16"
 }
 
 resource "aws_subnet" "test-a" {
